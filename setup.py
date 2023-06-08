78b9ecca4c708e8c30421648d58c1a3ad1e51b44
liopeer <lionel.peer@gmail.com>
2023-06-08 23:02:12 +0900

    changed module name

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     author="Lionel Peer",
     author_email="lionel.peer@gmail.com",
     url="https://github.com/liopeer/type_hints",
-    packages=["source"]
+    packages=["type_hints"]
 )
